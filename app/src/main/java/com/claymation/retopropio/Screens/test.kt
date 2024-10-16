d0f521e3b079eee140b1957f666baae16f9f903c
Joel Vargas Reynoso <76189923+joelvshimself@users.noreply.github.com>
2024-10-15 19:26:19 -0600

    test

--- app/src/main/java/com/claymation/retopropio/Screens/test.kt
+++ app/src/main/java/com/claymation/retopropio/Screens/test.kt
@@ -1,11 +1,10 @@
 package com.claymation.retopropio.Screens
 
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import android.widget.Toast
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,19 +12,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.NavController
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.claymation.retopropio.Viewmodels.ViewModel as MyViewModel
 
-
 @Composable
-fun test(navController: NavController?) {
-    Column {
-        SendEmailComposable()
-    }
+fun Test(){
+    SendEmailComposable()
 }
 
 @Composable
@@ -55,19 +49,5 @@ fun SendEmailComposable() {
             Text(text = "Send Email")
         }
     }
-}
-
-
-fun sendEmail(context: Context, userEmail: String) {
-    val senderEmail = "smikerivera@gmail.com"  // Sender's email
-    val password = "vkhd zuim skkq rqlm"       // App password (use the correct app password)
-    val recipientEmail = "a00836995@tec.mx"    // Recipient's email
-    val subject = "Nuevo Caso Recibido"
-
-    // Use the user's email in the body of the email
-    val body = "Hola abogado, llego un nuevo caso de $userEmail"
-
-    // Sending the email
-    SendEmailTask(senderEmail, password, recipientEmail, subject, body).execute()
 }
 
